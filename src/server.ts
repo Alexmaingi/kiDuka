835408375b04c3e7a90bc3cd47bd247d6eedf5b8
Joy Mary <jmkirongo@gmail.com>
2023-05-11 03:18:53 +0300

    adds user routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,15 @@
 import express, {json} from 'express'
-import userRoute from './Routes/userRoute'
+import userRoutes from './Routes/userRoutes'
 import productRoutes from './Routes/productRoutes'
 
 const app = express()
 app.use(json())
 
 
-app.use('/users', userRoute)
+app.use('/users', userRoutes)
 app.use('/products',productRoutes)
 
 
 app.listen(4000, ()=>{
     console.log("Server Running...")
 })
-import userRoutes from "./Routes/userRoutes";
-import express, { json } from "express";
-
-const app = express();
-app.use(json()); // middleware
-
-app.use("/user", userRoutes);
-
-app.listen(5000, () => {
-  console.log("Server Running...");
-});
